5370e6fd3c1e275d6fcefe4f6c92844f4ffa7692
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-20 19:32:57 +0530

    problems

--- day 1/problems.py
+++ day 1/problems.py
@@ -141,3 +141,109 @@ print(z)
 
 #unpacking a collection
 #If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#problem19(global variable)
+x = "great"
+def myfunc():
+  print("Python is " +x)
+myfunc()
+
+#problem20(local variable)
+x = "awesome"
+def myfunc():               
+  x = "fantastic"
+  print("Python is " + x)   
+
+myfunc()
+print("Python is " + x) 
+
+#problem21
+def myfunc():
+  global x
+  x = "fantastic"   
+myfunc()
+print("Python is " + x)
+
+#problem22
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x) 
+
+#problem23
+a = "vattapu"
+b = "bhargavi"
+print(a + " " + b)
+
+#problem24
+is_student = True
+print(is_student)
+print(type(is_student))
+
+#problem25
+a = 10
+#integer to float
+b = float(a)
+print(b)
+print(type(b))
+#float to integer
+x=15.5
+z=int(x)
+print(z)
+print(type(z))
+#integer to string
+a=10
+b=str(a)
+print(b)
+print(type(b))
+
+#problem26(si = (P*R*T)/100)
+principal = 1000
+rate = 5
+time = 2
+simple_interest = (principal * rate * time) / 100
+print("Simple Interest:", simple_interest)
+
+#problem27(temp celcius to fahrenheit)(F=9/5*C+32)
+celsius = 30
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
+
+#problem28(temp fahrenheit to celcius)(C=5/9*(F-32))
+fahrenheit = 86 
+celsius = (fahrenheit - 32) * 5/9
+print("Temperature in Celsius:", celsius)   
+
+#problem29
+python_marks = 85
+java_marks = 90
+dbms_marks = 80
+highest_marks = max(python_marks, java_marks, dbms_marks)
+print("Highest marks:", highest_marks)
+
+#problem30(total bill)
+item_name = "pen"
+quantity = 10
+price_per_item = 2.5
+total_bill = quantity * price_per_item
+print("Total bill:", total_bill)
+
+#problem31
+weight = 70
+height = 1.75   
+bmi = weight / (height ** 2)
+print("BMI:", bmi)
+
+#problem32
+current_year = 2026
+birth_year = 2005
+age = current_year - birth_year
+print(age)
+
+

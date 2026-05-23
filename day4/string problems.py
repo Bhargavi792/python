6749b90494db9d0cbdba9a72bc7024e968aa817a
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-23 16:36:23 +0530

    string problems

--- day4/string problems.py
+++ day4/string problems.py
@@ -132,6 +132,23 @@ for ch in text:
 
 print(result)
 
+#problem20
+text = "aaabbc"
+
+result = ""
+count = 1
+
+for i in range(len(text)-1):
+    if text[i] == text[i+1]:
+        count += 1
+    else:
+        result += text[i] + str(count)
+        count = 1
+
+result += text[-1] + str(count)
+
+print(result)
+
 
 
 

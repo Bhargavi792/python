ebb5f4a72740f70c8a29eb5af090a75155a80b3d
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-24 16:53:08 +0530

    slicing problems

---
+++ day5/slicing string problems.py
@@ -0,0 +1,30 @@
+#problem1
+
+text = "bhargavi"
+print(text[:3])
+
+#problem2
+text = "bhargavi"
+print(text[-4:])
+
+#problem3
+text = "bhargavi"
+print(text[::-1])
+
+#problem4
+text = "bhargavi"
+print(text[::2])
+
+#problem5
+text = "wonderfull"
+print(text[1:-1])
+
+#problem6
+text = "language"
+print(text[2:7])
+
+#problem7
+text = "bhargavi"
+print(text[-3:1])
+
+#problem8

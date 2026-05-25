9f8c2bb5b99d8a522e10a323d6a0a3438f3dde77
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-25 18:08:48 +0530

    problems

--- day6/problems.py
+++ day6/problems.py
@@ -36,3 +36,35 @@ print(text.capitalize())
 text = "hello world"
 
 print(text.upper())
+
+#problem8
+text = "python is easy"
+
+print(text.replace(" ", "-"))
+
+#problem9
+text = "Python"
+
+print(text[::-1])
+
+#problem10
+text = "Python"
+
+print(text.startswith("Py"))
+
+#problem11
+text = "programming"
+
+print(text.endswith("ing"))
+
+#problem12
+text = "Python is powerful"
+
+print(text.split())
+
+#problem13
+words = ["Python", "is", "easy"]
+
+print(" ".join(words))
+
+#problem14

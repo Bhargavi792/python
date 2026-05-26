2dcd46fc55c145e236c0fdb02be3325bdea897cf
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-26 14:26:40 +0530

    problems

---
+++ day7/problems.py
@@ -0,0 +1,48 @@
+#problem1
+a = "hello world"
+print("hello\nworld")
+
+#problem2
+a = "python programming"
+print("python\tprogramming")
+
+#problem3
+print("She said,\"python is easy\"")
+
+#problem4
+print("This is a backlash:\\")
+
+#problem5
+print("c:\\user\\bhargavi\\documents")
+
+#problem6
+print("name:bhargavi\ncity:hyderabad\nstate:telangana")
+
+#problem7
+print("*\n**\n***\n****")
+
+#problem8
+print("he said,\"i'm learning python.\"")
+
+#problem9
+floder_path ="D:\Python\Projects\Test"
+print("D:\\Python\\Projects\\Test")
+
+#problem10
+print("Name\tMarks\nRam\t90")
+
+#problem11
+print("\"Python\"\n\\tis\n\\\\awesome\\\\")
+
+#problem12
+print("Item\tPrice\nApple\t50\nMango\t80\nTotal\t130")
+
+#problem13
+print("Teacher: \"What is Python?\"\nStudent: \"It is a programming language.\"")
+
+#problem14
+print("A\nA B\nA B C\nA B C D")
+
+#problem15
+print("Name: \"Bhargavi\"\nPath: C:\\NewFolder\\Test\nStatus: Completed")
+

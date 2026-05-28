6e313eff04303e499348f64070e396f62226ccf1
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-28 14:31:42 +0530

    problems

---
+++ day9/string_method_prob.py
@@ -0,0 +1,42 @@
+#problem1
+text = "python"
+print(text.upper())
+
+#problem2
+text = "PYTHON"
+print(text.lower())
+
+#problem3
+text = "python programming"
+print(text.capitalize())
+
+#problem4
+text = "banana"
+print(text.count("a"))
+
+#problem5
+text = "hello world"
+print(text.find("world"))
+
+#problem6
+text = "I Love Python"
+print(text.replace("Python","Java"))
+
+#problem7
+text = "   hello   "
+print(text.strip())
+
+#problem8
+text = "python"
+print(text.startswith("py"))
+
+#problem9
+website = "google.com"
+print(website.endswith(".com"))
+
+#problem10
+text = "Python is easy"
+
+print(text.split())
+
+#problem11

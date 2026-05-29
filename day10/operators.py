ee913d04586202d41e32064672c16e3361bf3219
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-29 20:30:25 +0530

    operators

--- day10/operators.py
+++ day10/operators.py
@@ -35,5 +35,32 @@ print(x)
 
 
 #comparison operator
+==	Equal	x == y	
+!=	Not equal	x != y	
+>	Greater than	x > y	
+<	Less than	x < y	
+>=	Greater than or equal to	x >= y	
+<=	Less than or equal to	x <= y	
+
+#Logical operators
+and    	Returns True if both statements are true	                        x < 5 and  x < 10	
+or	    Returns True if one of the statements is true	                    x < 5 or x < 4	
+not 	Reverse the result, returns False if the result is true	not        (x < 5 and x < 10)	
+
+
+#Identity operators
+in 	    Returns True if a sequence with the specified value is present in the object	       x in y	
+not in	Returns True if a sequence with the specified value is not present in the object	   x not in y	
+
+#BITWISE operators
+& 	AND	Sets each bit to 1 if both bits are 1	x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	x | y	
+^	XOR	Sets each bit to 1 if only one of two bits is 1	x ^ y	
+~	NOT	Inverts all the bits	~x	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	x << 2	
+>>	Signed right shift	Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	x >> 2	
+
+
+
 
 '''

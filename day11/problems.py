5ad1b728f147ac1b46064a42c4b717fc73b831ff
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-30 21:53:44 +0530

    problems

--- day11/problems.py
+++ day11/problems.py
@@ -160,3 +160,11 @@ else:
 
 
 #problem 25
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+

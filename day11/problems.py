914cdf38fb31f8a2250b0b61cd02b675b4cc167f
Bhargavi792 <bhargavivattapu@gmail.com>
2026-05-30 13:08:59 +0530

    problems

---
+++ day11/problems.py
@@ -0,0 +1,42 @@
+a = 15
+b = 10
+print("Addition", a + b)
+
+#problem2
+a = 50
+b = 18
+print("Subtraction", a - b)
+
+#problem3
+a = 12
+b = 5
+print("Multiplication", a * b)
+
+#problem4
+a = 25
+b = 4
+print("Division",a / b)
+
+#problem5
+a = 25
+b = 6
+print("Modulus", a % b)
+
+#problem6
+a = 5
+b = 3
+print("Exponentiation", a ** b)
+
+#problem 7
+a = 17
+b = 3
+print("Floor Division", a // b)
+
+#problem 8
+a = 12
+b = 10
+c = 2
+d = 8 
+print("Arithmetic expression", a * b // c + d)
+
+#problem 9

c9c1439d945ae28d143516584cc633f937ceddea
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-04 22:46:37 +0530

    list sort

---
+++ day16/sort list.py
@@ -0,0 +1,36 @@
+'''
+Sort List Alphanumerically
+List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+
+ExampleGet your own Python Server
+Sort the list alphabetically:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort()
+print(thislist)
+
+->Example
+Sort the list numerically:
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+
+->Sort Descending
+To sort descending, use the keyword argument reverse = True:
+
+Example
+Sort the list descending:
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
+Example
+Sort the list descending:
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse = True)
+print(thislist)
+
+'''

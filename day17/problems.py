0944ea56d74cf499dae14801a8fad557ae61ef8c
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-05 14:00:25 +0530

    list problem access

--- day17/problems.py
+++ day17/problems.py
@@ -1,3 +1,47 @@
 #problem1
 mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
 print(mylist)
+
+#problem2
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[-1])
+
+#problem3
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[2])
+
+#problem4
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[3])
+
+#problem5
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[1:4])
+
+#problem 6
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[::2])
+
+#problem 7
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya"]
+print(mylist[-1])
+print(mylist[-2])
+print(mylist[-3])
+print(mylist[-4])
+print(mylist[-5])
+
+#problem 8
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya" , "grapes"]
+
+middle = len(mylist) // 2
+
+print(mylist[middle])
+
+#problem 9
+mylist = [ "apple" , "banana" , "mango" , "lichi" , "papaya" , "grapes"]
+
+print("first:", mylist[0])
+print("middle:", mylist[len(mylist )// 2])
+print("last:", mylist[-1])
+
+#problem 10

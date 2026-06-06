4416e0ee75db889bb371a2d37961daec80763529
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-06 12:40:30 +0530

    problems of list add

---
+++ day17/prob_list_add.py
@@ -0,0 +1,37 @@
+#problem1
+numbers = [ 2 , 4 , 8, 9]
+
+numbers.append(6)
+
+print(numbers)
+
+#problem 2
+numbers = [ 2 , 4 , 8, 9]
+
+numbers.extend([6 , 3 ])
+
+print(numbers)
+
+#problem 3
+numbers = [ 2 , 4 , 8, 9]
+
+numbers.insert(2,"bharu")
+
+print(numbers)
+
+#problem 4
+numbers = [ 2 , 4 , 8, 9]
+
+numbers.insert(0,"bharu")
+
+print(numbers)
+
+#problem 5
+list1 = [ 2, 3 ]
+list2 = [  8 ,6 ,4]
+
+list1.append(list2)
+print(list1)
+
+
+

e084287e4e2393ca158804faafd6c535daa3012c
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-07 21:57:38 +0530

    problems

---
+++ day19/problems.py
@@ -0,0 +1,37 @@
+#problem 1
+a = 5
+b = 4.5
+c = "bhargavi"
+d = True
+print("integer:",a)
+print("Float:",b)
+print("string:",c)
+print("boolean:",d)
+
+#problem 2
+a = 5
+b = 4.5
+c = "bhargavi"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+#problem 3
+a = 5
+c = float(a)
+print(c)
+
+#problem 4
+a = 5.6
+c = int(a)
+print(c)
+
+#problem 5
+a = 5
+c = str(a)
+print(c)
+
+#problem 6

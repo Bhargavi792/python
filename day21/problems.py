378f97937abfbb7ca1166c1f880175085c822ac0
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-09 22:45:47 +0530

    problems

---
+++ day21/problems.py
@@ -0,0 +1,41 @@
+num = 10
+
+result = float(num)
+
+print(result)
+print(type(result))
+
+
+#problem 2
+num = 10.75
+
+result = int(num)
+
+print(result)
+print(type(result))
+
+#problem 3
+num = 100
+
+result = str(num)
+
+print(result)
+print(type(result))
+
+#problem 4
+num = "200"
+
+result = int(num)
+
+print(result)
+print(type(result))
+
+#problem 5
+num = "25.5"
+
+result = float(num)
+
+print(result)
+print(type(result))
+
+#problem 5

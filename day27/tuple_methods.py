5d352782b15110cac5f12d78fa1e4a65cc950a91
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-15 22:20:15 +0530

    tuple

---
+++ day27/tuple_methods.py
@@ -0,0 +1,22 @@
+#problem1
+empty_tuple = ()
+single_item = (1,)
+coordinates = (10, 20 , 30)
+mixed_tuple =(1 , "hello" , 2.5)
+from_constructor = tuple([1, 2, 3])
+from_string = tuple("hello")
+
+#accesing  elements
+coordinaters = (10, 20, 30)
+print(coordinaters[0])
+print(coordinaters[-1])
+
+# slicing works like lists
+print(coordinaters[1:])
+
+# only two methods
+values = ( 2, 4, 5,)
+print(values.count(1))
+print(values.index(2))
+
+#problem

2293f7021f9070e98966591f20d7dbaa3d806893
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-15 21:37:19 +0530

    tuple

---
+++ day27/tuple.py
@@ -0,0 +1,29 @@
+'''
+# tuple
+A tuple in Python is an ordered, immutable collection of elements.
+It is one of Python’s sequence data types, along with lists and strings.
+
+1.ordered
+-> order of element is preserved
+-> elements can be accessed by their index
+
+2.immutable
+->cannot be changed , added , removed.
+->this makes tuple hashable()if all are hashable they can used in dictionary keys
+
+3.tuple is created using()
+
+methods
+empty_tuple = ()
+single_item = (1,)
+coordinates = (10, 20 , 30)
+mixed tuple =(1 , "hello" , 2.5)
+from_constructor = tuple([1, 2, 3])
+from_string = tuple("hello")
+
+tuple size and membership 
+print(len(coordinates))
+print(20 in coordinates)
+
+
+'''

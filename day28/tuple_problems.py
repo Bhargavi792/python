f26a7773eb73bd9df0b67167acb6040e667b88fa
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-16 23:23:49 +0530

    tuple

---
+++ day28/tuple_problems.py
@@ -0,0 +1,15 @@
+# Problematic: Creates an integer
+not_a_tuple = (5)  # type: int
+
+# Correct: Creates a tuple
+a_tuple = (5,)  # type: tuple
+
+#
+# Modifying Immutability (TypeError)Problem: Attempting to add, remove, or change elements raises a TypeError.Cause: Tuples are strictly immutable sequences.Solution: Convert the tuple to a mutable list, make modifications, and convert it back
+data = (10, 20, 30)
+
+# Workaround to "append" or modify
+temp_list = list(data)
+temp_list.append(40)
+data = tuple(temp_list)  # Output: (10, 20, 30, 40)
+

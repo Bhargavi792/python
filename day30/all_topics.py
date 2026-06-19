fedbf76db97cd2494f0267acaa4763d8fdc1f0b2
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-19 23:11:12 +0530

    all concepts

--- day30/all_topics.py
+++ day30/all_topics.py
@@ -14,16 +14,17 @@ print("bharagvi",vr)
 vr = int(input("integer value"))
 print(vr)
 
+
 #float
 vr = float(input("float value"))
 print(vr)
 
 #range function (returns a sequence )  (but int_stop_value is the compulsory parameter in range function)
 #eg :0 to n-1 for range(0,n)
-range(int_start_value,int_stop_value,int_step_value)
+#range(int_start_value,int_stop_value,int_step_value)
 
 #eg all even numbers from 1 to 100
 for i in range(0,101,2):
         print(i)
 
-        
+

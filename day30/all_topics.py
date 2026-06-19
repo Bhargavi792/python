d5fcc1d4d1c65484c211987ae4712ba55c715fc1
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-19 23:06:05 +0530

    all concepts

---
+++ day30/all_topics.py
@@ -0,0 +1,29 @@
+#basic 
+print(".....")
+  
+
+#method 2
+vr = "bhargavi"
+print("hi this is",vr)
+
+#takin input from user
+vr = input("enter your game")
+print("bharagvi",vr)
+
+#dataype input (int)
+vr = int(input("integer value"))
+print(vr)
+
+#float
+vr = float(input("float value"))
+print(vr)
+
+#range function (returns a sequence )  (but int_stop_value is the compulsory parameter in range function)
+#eg :0 to n-1 for range(0,n)
+range(int_start_value,int_stop_value,int_step_value)
+
+#eg all even numbers from 1 to 100
+for i in range(0,101,2):
+        print(i)
+
+        

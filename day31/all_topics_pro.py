0494e8fd81861d23ab134f4a87a0dbee73122056
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-21 21:34:53 +0530

    practice

---
+++ day31/all_topics_pro.py
@@ -0,0 +1,44 @@
+#print("\n") ->new line
+
+#backslash
+print("\\")
+
+#octal value 
+print("\ooo")
+
+#hexa value
+print("\xhh")
+
+#carriage return (it returns to the first line of the cursor)
+print("\r")
+
+#strings
+a = "bhargavi"
+print("string is",a)
+
+#slicing
+#string_var[int_start_value:int_stop_value:int_step_value]
+
+#isalnum()
+#string_variable.isalnum()
+
+#isalpha()method
+#string_variable.isalpha()
+
+#isdecimal() method
+'''Returns True if all the characters in the string are decimals
+
+string_variable.isdecimal()
+'''
+
+'''
+1.isdigit (it checks the string is digit or not)
+2.islower(all characters are lower)
+3.isspace(true if all character have space)
+4.isupper()
+5.lower()->converts into  lowwer
+6.upper()
+7.strip() ->It removes leading and trailing spaces in the string
+
+'''
+
---
+++ day32/practice.py
@@ -0,0 +1,45 @@
+#list
+#var_name =[element1,element2,...]
+'''
+->indexing
+->list id ordered , indexed ,mutable and most flexible
+
+
+1.empty list
+my_list = []
+
+2.index method
+
+list.index(elements)
+
+3.append methods
+[add elements at the last]
+
+list.append(element)
+
+4.extend method
+we can add at the end of the current list for present list
+
+list.extend(element)
+
+5.insert method
+[any specific position]
+list.insert(position , element)
+
+6.pop method
+list.remove(element)
+
+clear method
+list.clear()
+
+
+reverse method 
+list.reverse()
+
+sort method
+list.sort(reverse=true|false)
+
+
+
+
+'''

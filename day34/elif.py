27c6fb5ccfa7ae6a1a2d5e94df769f4840bf9bc8
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-24 23:35:57 +0530

    elif statement

--- day34/elif.py
+++ day34/elif.py
@@ -29,4 +29,6 @@ elif score >= 60:
   Important: Only the first true condition will be executed. Even if multiple conditions are true, Python stops after executing the first matching block.
   
   
+
+  
 '''

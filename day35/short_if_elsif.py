fb51d433022986c6d43e32795ba619dbe631cf82
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-25 11:23:39 +0530

    short if

---
+++ day35/short_if_elsif.py
@@ -0,0 +1,32 @@
+'''
+Short Hand If
+If you have only one statement to execute, you can put it on the same line as the if statement.
+
+ExampleGet your own Python Server
+One-line if statement:
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+Note: You still need the colon : after the condition.
+
+#Short Hand If ... Else
+If you have one statement for if and one for else, you can put them on the same line using a conditional expression:
+
+Example
+One-line if/else that prints a value:
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+->The syntax follows this pattern:
+
+variable = value_if_true if condition else value_if_false
+
+-> sometime it is called as ternary opreations
+->Ternary operators are particularly useful for simple assignments and return statements.
+
+
+'''

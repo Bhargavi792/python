0ba057af0a4fee1279ba0813f305108390a97cdb
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-25 11:04:21 +0530

    elif statement

---
+++ day35/elif.py
@@ -0,0 +1,28 @@
+'''
+when to us elif 
+-> when there are many conditions to check (mutiple mutally opreations to check)
+-> if we use if statements at this condition python will consider on;y one true statment
+
+example
+Day of the week checker:
+
+day = 3
+
+if day == 1:
+  print("Monday")
+elif day == 2:
+  print("Tuesday")
+elif day == 3:
+  print("Wednesday")
+elif day == 4:
+  print("Thursday")
+elif day == 5:
+  print("Friday")
+elif day == 6:
+  print("Saturday")
+elif day == 7:
+  print("Sunday")
+
+  
+  
+'''

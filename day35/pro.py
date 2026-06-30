8cc49c88bdbec8ab910d14f48f65aef88942560d
Bhargavi792 <bhargavivattapu@gmail.com>
2026-06-30 22:31:31 +0530

    previous concepts

--- day35/pro.py
+++ day35/pro.py
@@ -4,4 +4,19 @@ Variables are containers for storing data values.
 
 A variable is created the moment you first assign a value to it.
 
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+
+Casting
+If you want to specify the data type of a variable, this can be done with casting.
+
+
 '''

869ce23a20897a2e3abb0c683a65791fa3164c89
Bhargavi792 <bhargavivattapu@gmail.com>
2026-07-01 22:23:49 +0530

    problems

---
+++ day36/problems.py
@@ -0,0 +1,60 @@
+1.
+name = "bhargavi"
+print("name")
+
+2.
+age = 20
+print("age")
+
+3.
+a = 10
+b = 20
+sum = a+b
+print(sum)
+
+4.
+college = "scient institute of technology"
+print(college)
+
+5.
+a = "city"
+b = "state"
+c = "country"
+print(a,b,c)
+
+6.
+price = 30.5
+print(type(price))
+
+7.
+is_student = True
+print(is_student)
+
+8.
+a = 30
+float_num = float(a)
+print(float_num)
+
+9.
+b = 20.8
+integer_num = int(b)
+print(integer_num)
+
+10.
+a = 2
+square = 2**2
+print(square)
+
+11.
+length = 15
+width = 8
+area = length*width
+print(area)
+
+12.
+celsius = 25
+fahrenheit = (celsius*9/5)+32
+print(fahrenheit)
+
+
+
